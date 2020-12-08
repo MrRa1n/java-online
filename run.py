6af5b8984d8422b6fb97e75cf247059042fcf258
Toby Cook <tobycookni@gmail.com>
2020-12-08 15:27:26 +0000

    Removed gitignore and run file

--- run.py
+++
@@ -1,8 +0,0 @@
-from javaonline import app, read_config, configure_logger
-
-print(app.url_map)
-
-if __name__ == '__main__':
-    read_config(app)
-    configure_logger(app)
-    app.run(debug=True, use_reloader=False)
